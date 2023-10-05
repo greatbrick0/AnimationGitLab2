5437593096ed53d1369418bd87d9a7902e87ff2c
greatbrick <speharley@gmail.com>
2023-10-04 23:58:01 -0400

    Added animation

--- Lab2/AlgoAnimLab2/Assets/PathController.cs
+++ Lab2/AlgoAnimLab2/Assets/PathController.cs
@@ -15,6 +15,8 @@ public class PathController : MonoBehaviour
     [HideInInspector]
     public int blockingObjectCount = 0;
 
+    [SerializeField] bool instantRotate = false;
+
     private void Start()
     {
         thePath = pathManager.GetPath();
@@ -26,11 +28,18 @@ public class PathController : MonoBehaviour
 
     void RotateTowardsTarget()
     {
-        float stepSize = rotateSpeed * Time.deltaTime;
+        if (instantRotate)
+        {
+            return;
+        }
+        else
+        {
+            float stepSize = rotateSpeed * Time.deltaTime;
 
-        Vector3 targetDir = target.pos - transform.position;
-        Vector3 newDir = Vector3.RotateTowards(transform.forward, targetDir, stepSize, 0.0f);
-        transform.rotation = Quaternion.LookRotation(newDir);
+            Vector3 targetDir = target.pos - transform.position;
+            Vector3 newDir = Vector3.RotateTowards(transform.forward, targetDir, stepSize, 0.0f);
+            transform.rotation = Quaternion.LookRotation(newDir);
+        }
     }
 
     void MoveForward()
@@ -41,8 +50,22 @@ public class PathController : MonoBehaviour
         {
             return;
         }
-        Vector3 moveDir = Vector3.forward;
-        transform.Translate(moveDir * stepSize);
+
+        Vector3 moveDir = Vector3.zero;
+        if (instantRotate)
+        {
+            moveDir = (target.pos - transform.position).normalized;
+            transform.position += moveDir * stepSize;
+            if (Vector3.Distance(transform.position, target.pos) < 0.1f)
+            {
+                target = pathManager.GetNextPoint();
+            }
+        }
+        else
+        {
+            moveDir = Vector3.forward;
+            transform.Translate(moveDir * stepSize);
+        }
     }
 
     private void Update()
@@ -53,7 +76,7 @@ public class PathController : MonoBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
-        if (other.gameObject.CompareTag("Waypoint"))
+        if (other.gameObject.CompareTag("Waypoint") && !instantRotate)
         {
             target = pathManager.GetNextPoint();
         }
