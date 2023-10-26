5da6ce3574a16a166e03859b42131625e8c8ddf7
greatbrick <speharley@gmail.com>
2023-10-25 21:50:08 -0400

    Added Some Boss Behaviour

--- Lab3/Assets/BossScript.cs
+++ Lab3/Assets/BossScript.cs
@@ -1,18 +1,67 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class BossScript : MonoBehaviour
 {
-    // Start is called before the first frame update
+    NavMeshAgent agent;
+
+    [SerializeField]
+    Transform target;
+
+    int hatCount = 3;
+    bool stunned = false;
+
     void Start()
     {
-        
+        agent = GetComponent<NavMeshAgent>();
     }
 
-    // Update is called once per frame
+
     void Update()
     {
-        
+        agent.SetDestination(target.position);
+        agent.speed = stunned ? 0 : 1;
+    }
+
+    public void Hit()
+    {
+        print("hit");
+        target.GetComponent<PlayerScript>().Jump(7);
+
+        if(hatCount == 1)
+        {
+            stunned = true;
+        }
+        else if(hatCount == 0)
+        {
+            Destroy(this.gameObject);
+        }
+
+        if(hatCount > 0)
+        {
+            hatCount -= 1;
+            UpdateHats();
+        }
+    }
+
+    void UpdateHats()
+    {
+        transform.GetChild(0).gameObject.SetActive(false);
+        transform.GetChild(1).gameObject.SetActive(false);
+        transform.GetChild(2).gameObject.SetActive(false);
+        if (hatCount >= 1)
+        {
+            transform.GetChild(0).gameObject.SetActive(true);
+        }
+        if (hatCount >= 2)
+        {
+            transform.GetChild(1).gameObject.SetActive(true);
+        }
+        if (hatCount >= 3)
+        {
+            transform.GetChild(2).gameObject.SetActive(true);
+        }
     }
 }
---
+++ Lab3/Assets/PlayerScript.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour
+{
+    Rigidbody rb;
+    [SerializeField]
+    float jumpForce = 4;
+    [SerializeField]
+    float speed = 5;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        Vector2 inputDir = Vector2.zero;
+        if (Input.GetKey(KeyCode.W)) inputDir += Vector2.up;
+        if (Input.GetKey(KeyCode.S)) inputDir += Vector2.down;
+        if (Input.GetKey(KeyCode.A)) inputDir += Vector2.left;
+        if (Input.GetKey(KeyCode.D)) inputDir += Vector2.right;
+
+        if (Input.GetKeyDown(KeyCode.Space)) Jump(jumpForce);
+
+        rb.velocity = new Vector3(inputDir.x * speed, rb.velocity.y, inputDir.y * speed);
+    }
+
+    public void Jump(float amount)
+    {
+        rb.velocity = new Vector3(rb.velocity.x, amount, rb.velocity.z);
+    }
+}
